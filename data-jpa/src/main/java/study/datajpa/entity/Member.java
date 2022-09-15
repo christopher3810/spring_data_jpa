b5ed5e3ede6311034cb5aeb4ff06c0e358728e3a
sungminyang <lonesome661@naver.com>
2022-09-16 01:42:00 +0900

    member, team entity 로직추가 및 entity test 코드 작성

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,27 +1,53 @@
 package study.datajpa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-        //entity는 parameter 없이 default 생성자가 있어야되고
-        //protected로 열어 둬야함
-        //프록시 가술 사용시 jpa 구현체들이 객체를 강제로 만들어야 하는데
-        //그때 객체 생성시 막힘 표준 스펙이 적혀 있음
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+            "id = " + id + ", " +
+            "username = " + username + ", " +
+            "age = " + age + ")";
+    }// 연관관계 있는 항목은 toString을 하지 않기 타고들어가서 무한루프 걸릴수도 있음
 }
