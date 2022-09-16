2ce4e27d728f5ee5de81dd2eeddc79d82cd2ce31
sungminyang <lonesome661@naver.com>
2022-09-17 00:08:18 +0900

    Spring Data Jpa Repository활용해서 test 수정

--- data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,38 +1,9 @@
 package study.datajpa.repository;
 
-import java.util.List;
-import java.util.Optional;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Team;
 
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Team save(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team) {
-        em.remove(team);
-    }
-
-    public List<Team> findAll() {
-        return em.createQuery("select t from Team t", Team.class).getResultList();
-    }
-
-    public Optional<Team> findById(Long id) {
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team); //null 일수도 아닐수도 있드아
-    }
-
-    public long count() {
-        return em.createQuery("select count(t) from Team t", Long.class).getSingleResult();
-    }
+//@Repository 애너테이션은 생략도가능 jpa interface 보고 체크함
+public interface TeamRepository extends JpaRepository<Team, Long> {
 
 }
