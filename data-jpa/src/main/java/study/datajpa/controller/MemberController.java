f8ebd5bb9c24520f9f6953d5249fe61b2e241f2b
sungminyang <lonesome661@naver.com>
2022-10-11 00:07:23 +0900

    Spring Data Jpa를 활용한 Audit 구현, 도메인 클래스 컨버터 사용 코드 구현

---
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,34 @@
+package study.datajpa.controller;
+
+import javax.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id){
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/membersDomain/{id}")
+    public String findMember(@PathVariable("id") Member member){
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init(){
+        memberRepository.save(new Member("userA"));
+    }
+
+}
--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -23,7 +23,7 @@ import lombok.Setter;
     query="select m from Member m where m.username = :username"
 )
 @NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
-public class  Member extends JpaBaseEntity{
+public class  Member extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "member_id")
